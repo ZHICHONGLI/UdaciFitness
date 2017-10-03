0db1dcd544d16b9112fd4b30247d0e0e41dfc7a1
Zhichong Li <leezhc@gmail.com>
2017-10-02 23:13:08 -0700

    check

--- App.js
+++ App.js
@@ -1,31 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import React from 'react'
+import { View } from 'react-native'
+import AddEntry from './components/AddEntry'
 
 export default class App extends React.Component {
-  componentDidMount() {
-    // console.log('before');
-    // debugger
-    // console.log('after')
-  }
-  
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Ionicons name='logo-apple' color='grey' size='100'/>
+      <View>
+        <AddEntry />
       </View>
-    );
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

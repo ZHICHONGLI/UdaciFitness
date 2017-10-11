92fd29ac0daac1124b0e0fb0396bc9e41f48999e
Zhichong Li <leezhc@gmail.com>
2017-10-10 23:34:40 -0700

    check

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import React from 'react'
-import { View, Text, StyleSheet, TouchableHighlight, TouchableNativeFeedback
-  ,TouchableOpacity, TouchableWithoutFeedback } from 'react-native'
-import AddEntry from './components/AddEntry';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
-import reducer from './reducers';
+import { View } from 'react-native'
+import AddEntry from './components/AddEntry'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import History from './components/History'
 
 export default class App extends React.Component {
-  handlePress = () => {
-    alert('hello')
-  }
   render() {
     return (
       <Provider store={createStore(reducer)}>
-        <View>
-          <AddEntry />
+        <View style={{flex: 1}}>
+          <History />
         </View>
       </Provider>
     )
